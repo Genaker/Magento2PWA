fe172a23996f6150cfb1ec5045903fba591dcd38
Egor Shitikov <egorshitikov@gmail.com>
2021-08-13 15:23:12 -0700

    Update Settings.php

--- ViewModel/Settings.php
+++ ViewModel/Settings.php
@@ -18,9 +18,11 @@ class Settings extends \Magento\Framework\DataObject implements \Magento\Framewo
      * Settings constructor.
      *
      */
-    public function __construct(\Magento\Framework\App\ResourceConnection $connection,
-                                \Magento\Store\Model\StoreResolver $storeResolver,
-                                \Magento\Store\Model\StoreManagerInterface $storeManager)
+    public function __construct(
+        \Magento\Framework\App\ResourceConnection $connection,
+        \Magento\Store\Model\StoreResolver $storeResolver,
+        \Magento\Store\Model\StoreManagerInterface $storeManager
+    )
     {
         $this->connection = $connection->getConnection();
         $this->storeManager = $storeManager;
@@ -38,7 +40,7 @@ class Settings extends \Magento\Framework\DataObject implements \Magento\Framewo
         // $viewModel = $block->getData('viewModel');
         // echo $viewModel->getSettings();
         
-        return __('Hello Developer!');
+        return [];
     }
 
     public function getPWAConfig(){
@@ -51,26 +53,25 @@ class Settings extends \Magento\Framework\DataObject implements \Magento\Framewo
         $tableName = $this->connection->getTableName('core_config_data');
         $storeId = 0; // $this->getCurrentStoreId();
        
-        //ToDo: Magent multi store functionality is broken than we will not use scoped config and I'm not recommentding to use use magento multistore 
+        //ToDo: Magento multi store functionality is broken and we will not use scoped config and I'm not recommentding to use use magento multistore 
         $config = $this->connection->fetchAll("SELECT path,value FROM $tableName where scope_id in (0 , $storeId) and path like \"pwa/pwa_settings%\"");
         $normal = [];
+        // If you need  Multi/Separate store PWA you can immplement it yorself.
         //var_dump($config);
      
-        //header('Content-Type: application/json');
-        //if (config[''])
 
         foreach ($config as $row){
-            $normal[explode('/',$row['path'])[2]] = $row['value'];
+            $normal[explode('/', $row['path'])[2]] = $row['value'];
            
         }
         // ToDo: This feature is not tested
-        if ($normal['manifest_json'] !== null && strlen($normal['manifest_json']) > 10){
+        if ($normal['manifest_json'] !== null && strlen($normal['manifest_json']) > 10) {
             $normal =  json_decode($normal['manifest_json']);
         } else {
-        unset($normal['manifest_json']);
-        $normal['icons'] = json_decode($normal['icons']);
+            unset($normal['manifest_json']);
+            $normal['icons'] = json_decode($normal['icons']);
         }     
-        var_dump($normal);
+
         // validate manifest you can here - https://manifest-validator.appspot.com/
         echo(json_encode($normal));
         return $normal;
