8e8cc29eaa492bd26117ba52cb95337f6821c8c8
Egor Shitikov <egorshitikov@gmail.com>
2021-08-15 19:07:33 -0700

    Update UpgradeData.php

--- Setup/UpgradeData.php
+++ Setup/UpgradeData.php
@@ -31,7 +31,7 @@ class UpgradeData implements UpgradeDataInterface
      * @param ModuleDataSetupInterface $setup
      * @param ModuleContextInterface $context
      */
-    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
         $setup->startSetup();
  
